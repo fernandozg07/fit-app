a638632539bf235eddc66239cabd9f51b601c7ed
fernandozggg <fernandozaninigalletti@gmail.com>
2025-04-08 16:52:23 -0300

    Cria superusuário automaticamente no deploy

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,19 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError
+        from django.db import ProgrammingError
+        try:
+            User = get_user_model()
+            if not User.objects.filter(email="fernandozaninigalletti@gmail.com").exists():
+                User.objects.create_superuser(
+                    email="fernandozaninigalletti@gmail.com",
+                    password="fg020607"
+                )
+                print("✔️ Superusuário criado automaticamente.")
+        except (OperationalError, ProgrammingError):
+            # Ignora erro se o banco de dados ainda não estiver pronto
+            pass
