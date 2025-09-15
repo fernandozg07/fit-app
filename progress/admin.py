33eb55dd5855816adc6a0e5c514a068d065140d5
fernandozggg <fernandozaninigalletti@gmail.com>
2025-09-15 14:27:05 -0300

    feat: alteração do pateteu

--- progress/admin.py
+++ progress/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import ProgressEntry  # Corrija para ProgressEntry, não Progress
 
 admin.site.register(ProgressEntry)  # Registra o modelo no admin
-#dw
+#novo
